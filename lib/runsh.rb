dc560755dc8b51968742ea99c97e8fbb175864a3
TOKI Yoshinori <toki@freedom.ne.jp>
2015-06-28 19:56:52 +0900

    simple token scanner.

--- lib/runsh.rb
+++ lib/runsh.rb
@@ -3,6 +3,37 @@
 require 'runsh/version'
 
 module RunSh
+  SPECIAL_SEPARATORS = %w[
+    || |  &&  &
+    <  >
+    (( )) (  )  {{ }} { }
+    $(( $( ${{ ${ $
+    ` \\  "
+    ;
+  ]
+
+  SEPARATOR_SCAN_PATTEN = /
+    (?<word> .*?)
+    (?<separator>
+      (?:
+        #{SPECIAL_SEPARATORS.map{|s| Regexp.quote(s) }.join('|')} |
+        [\ \t]+ |
+        \n |
+        \Z
+      )
+    )
+  /mx
+
+  def scan_token(source_text)
+    token_list = []
+    source_text.scan(SEPARATOR_SCAN_PATTEN) do
+      token_list.push([ :word, $~[:word] ]) unless $~[:word].empty?
+      token_list.push([ :sep, $~[:separator] ]) unless $~[:separator].empty?
+    end
+
+    token_list
+  end
+  module_function :scan_token
 end
 
 # Local Variables:
--- test/test_runsh.rb
+++ test/test_runsh.rb
@@ -6,6 +6,40 @@ require 'test/unit'
 
 module RunSh::Test
   class RunShTest < Test::Unit::TestCase
+    def test_scan_token
+      assert_equal([], RunSh.scan_token(''))
+      assert_equal([ [ :word, 'test' ] ], RunSh.scan_token('test'))
+      assert_equal([ [ :sep, "  \t\t\t " ] ], RunSh.scan_token("  \t\t\t "))
+      assert_equal([ [ :word, 'test' ], [ :sep, "\n" ] ], RunSh.scan_token("test\n"))
+      assert_equal([ [ :word, 'ls' ],
+                     [ :sep, ' ' ],
+                     [ :word, '-ltr' ],
+                     [ :sep, ' ' ],
+                     [ :word, '*.txt' ],
+                     [ :sep, "\n" ]
+                   ], RunSh.scan_token("ls -ltr *.txt\n"))
+      assert_equal([ [ :sep, '((' ],
+                     [ :sep, ' ' ],
+                     [ :sep, '(' ], [ :word, '1' ], [ :sep, ' ' ], [ :word, '+' ], [ :sep, ' ' ], [ :word, '2' ], [ :sep, ')' ],
+                     [ :sep, ' ' ], [ :word, '*' ], [ :sep, ' ' ], [ :word, '3' ],
+                     [ :sep, ' ' ], [ :word, '/' ], [ :sep, ' ' ], [ :word, '4' ],
+                     [ :sep, ' ' ],
+                     [ :sep, '))' ]
+                   ], RunSh.scan_token('(( (1 + 2) * 3 / 4 ))'))
+      assert_equal([ [ :sep, '||' ], [ :sep, '|' ],
+                     [ :sep, '&&' ], [ :sep, '&' ],
+                     [ :sep, '<' ], [ :sep, '>' ],
+                     [ :sep, '((' ], [ :sep, '(' ],
+                     [ :sep, '))' ], [ :sep, ')' ],
+                     [ :sep, '{{' ], [ :sep, '{' ],
+                     [ :sep, '}}' ], [ :sep, '}' ],
+                     [ :sep, '$((' ], [ :sep, '))' ],
+                     [ :sep, '$(' ], [ :sep, ')' ],
+                     [ :sep, '${{' ], [ :sep, '}}' ],
+                     [ :sep, '${' ], [ :sep, '}' ],
+                     [ :sep, "\\" ], [ :sep, '$' ]
+                   ], RunSh.scan_token(%w[ ||| &&& <> ((( ))) {{{ }}} $(()) $() ${{}} ${} \\ $ ].join('')))
+    end
   end
 end
 
