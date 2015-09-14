1006aef461149e2b045621f6b5273af6e2c90be8
TOKI Yoshinori <toki@freedom.ne.jp>
2015-09-14 12:00:49 +0900

    syntax tree replace visitor

--- lib/runsh/parser.rb
+++ lib/runsh/parser.rb
@@ -58,6 +58,10 @@ module RunSh
       def accept(visitor)
         visitor.visit_cmd_list(self)
       end
+
+      def new
+        self.class.new(eoc: @eoc)
+      end
     end
 
     class FieldList
@@ -84,6 +88,10 @@ module RunSh
 
       def accept(visitor)
         visitor.visit_field_list(self)
+      end
+
+      def new
+        self.class.new
       end
     end
 
@@ -130,6 +138,10 @@ module RunSh
 
       def accept(visitor)
         visitor.visit_qq_list(self)
+      end
+
+      def new
+	self.class.new
       end
     end
 
@@ -378,6 +390,59 @@ module RunSh
         end
 
         @s
+      end
+    end
+
+    class CollectVisitor < Visitor
+      def initialize(context, cmd_intp, collection)
+        super(context, cmd_intp)
+        @collection = collection
+      end
+    end
+
+    class ReplaceVisitor < Visitor
+      def visit_cmd_list(cmd_list)
+        new_cmd_list = cmd_list.new
+        for field_list in cmd_list.fields
+          new_cmd_list.add(field_list.accept(self))
+        end
+
+        new_cmd_list
+      end
+
+      def visit_field_list(field_list)
+        new_field_list = field_list.new
+        for value in field_list.values
+          new_field_list.add(value.accept(self))
+        end
+
+        new_field_list
+      end
+
+      def visit_param_expan(parameter_expansion)
+        replace_holder = ReplaceHolder.new
+        collect = CollectVisitor.new(@c, @i, replace_holder)
+        parameter_expansion.accept(collect)
+
+        replace_holder
+      end
+
+      def visit_qq_list(qq_list)
+        new_qq_list = qq_list.new
+        collect = CollectVisitor.new(@c, @i, new_qq_list)
+        for value in qq_list.values
+          value.accept(collect)
+        end
+
+        new_qq_list
+      end
+
+      def visit_qs(qs)
+        qs
+      end
+
+      def visit_s(string)
+        string
       end
     end
 
